9b5e7a5cd8f8b4fe205930de60048bc82171dae5
bermu <bermu@ubuntu.(none)>
2011-06-07 18:42:11 +0200

    Problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -8,7 +8,15 @@ class Problem < ActiveRecord::Base
 	validates_presence_of :question, :message => 'Pregunta no puede estar en blanco'
 	validates :question, :length   => { :maximum => 2040, :message => 'Pregunta demasiada larga' }
 
-  has_attached_file :picture, :styles => { :thumb=> "100x100#", :small  => "150x150>" }, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :url  => '/pictures/:style/:basename.:extension', :path => 'pictures/:style/:basename.:extension', :bucket => 'albertorecurso'
+  has_attached_file :picture,
+       :styles => {
+       :thumb=> "100x100#",
+       :small  => "150x150>" },
+     :storage => :s3,
+     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+     :path => "/:style/:id/:filename"
 
 
 end
+
+
